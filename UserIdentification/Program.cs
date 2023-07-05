10a8f4a4e4587b42b6737341f2027d028d7a840f
zjk <2455650395@qq.com>
2023-07-05 10:46:53 +0800

    添加jwt鉴权工具

--- UserIdentification/Program.cs
+++ UserIdentification/Program.cs
@@ -1,6 +1,11 @@
 using UserIdentification.entity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using UserIdentification.utils;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
 
@@ -23,6 +28,28 @@ builder.Services.AddCors(options =>
         );
 });
 
+// 注册服务
+builder.Services.AddAuthentication(options =>
+{
+options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true, //是否验证Issuer
+            ValidIssuer = configuration["Jwt:Issuer"], //发行人Issuer
+            ValidateAudience = true, //是否验证Audience
+            ValidAudience = configuration["Jwt:Audience"], //订阅人Audience
+            ValidateIssuerSigningKey = true, //是否验证SecurityKey
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:SecretKey"])), //SecurityKey
+            ValidateLifetime = true, //是否验证失效时间
+            ClockSkew = TimeSpan.FromSeconds(30), //过期时间容错值，解决服务器端时间不同步问题（秒）
+            RequireExpirationTime = true,
+    };
+});
+builder.Services.AddSingleton(new JwtHelper(configuration));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -36,6 +63,7 @@ app.UseCors("myCors");
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
---
+++ UserIdentification/web/controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using UserIdentification.utils;
+
+namespace UserIdentification.web.controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AccountController : Controller
+    {
+        private readonly JwtHelper _jwtHelper;
+
+        public AccountController(JwtHelper jwtHelper)
+        {
+            _jwtHelper = jwtHelper;
+        }
+
+        [HttpGet]
+        public ActionResult<string> GetToken()
+        {
+            return _jwtHelper.CreateToken();
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult<string> GetTest()
+        {
+            return "Test Authorize";
+        }
+    }
+}
