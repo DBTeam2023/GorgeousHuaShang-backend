e070715bd5f9c7861a117d15d7b7e96b8b87ca95
sty <2969627331@qq.com>
2023-08-25 12:14:11 +0800

    feat:image service

--- UserIdentification/domain/service/impl/AvatarServiceImpl.cs
+++ UserIdentification/domain/service/impl/AvatarServiceImpl.cs
@@ -1,58 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using UserIdentification.exception;
+using UserIdentification.utils;
 
 namespace UserIdentification.domain.service.impl
 {
     public class AvatarServiceImpl : AvatarService
     {
+        public static string directoryPath = "C:\\Users\\Administrator\\Desktop\\image\\avatar";
+
+        ImageService imageService = new ImageService(directoryPath);
+
         public AvatarServiceImpl() { }
-
-        public string directoryPath = "C:\\Users\\Administrator\\Desktop\\image\\avatar";
-        //public string directoryPath = "C:\\Users\\admin\\Desktop\\DBPrinciple\\FinalProject\\testImage";
-
-        public string? Address { get; set; }
 
         public void setAvatar(IFormFile image, string avatarName)
         {
-            if(image == null)
-            {
-                throw new InvalidTypeException("null image");
-            }
-
-            string filename = avatarName + Path.GetExtension(image.FileName);
-            string imagePath = Path.Combine(directoryPath, filename);
-            try
-            {
-                using (var stream = new FileStream(imagePath, FileMode.OpenOrCreate))
-                {
-                    image.CopyTo(stream);  // save the picture
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new NotFoundException("directory not found");
-            }
-            
-            return;
+            imageService.setImage(image, avatarName);
         }
 
         public FileContentResult getAvatar(string avatarName)
         {
-            string filename = avatarName + ".png";
-            string imagePath = Path.Combine(directoryPath, filename); // file path
-
-            try
-            {
-                byte[] imageData = System.IO.File.ReadAllBytes(imagePath);
-                string contentType = "image/png"; // file type
-
-                return new FileContentResult(imageData, contentType);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                throw new NotFoundException("image file not found");
-            }
+            return imageService.getImage(avatarName);
         }
     }
 }
---
+++ UserIdentification/utils/ImageService.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserIdentification.exception;
+
+namespace UserIdentification.utils
+{
+    /**
+     * @author sty
+     * @brief image access
+     * 
+     * @param directory path
+     * @param image type
+     */
+    public class ImageService
+    {
+        public string directoryPath;
+        public string imageType = "png";
+
+        public ImageService(string directoryPath)
+        {
+            this.directoryPath = directoryPath;
+        }
+
+        public ImageService(string directoryPath, string imageType)
+        {
+            this.directoryPath = directoryPath;
+            this.imageType = imageType;
+        }
+
+        public void setImage(IFormFile image, string imageName)
+        {
+            if (image == null)
+            {
+                throw new InvalidTypeException("null image");
+            }
+
+            string fileExtension = Path.GetExtension(image.FileName);
+            if (fileExtension != "." + imageType)
+            {
+                throw new InvalidTypeException("invalid file extension");
+            }
+            string filename = imageName + fileExtension;
+            string imagePath = Path.Combine(directoryPath, filename);
+
+            try
+            {
+                using (var stream = new FileStream(imagePath, FileMode.OpenOrCreate))
+                {
+                    image.CopyTo(stream);  // save the picture
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new NotFoundException("directory not found");
+            }
+
+            return;
+        }
+
+        public FileContentResult getImage(string imageName)
+        {
+            string filename = imageName + "." + imageType;
+            string imagePath = Path.Combine(directoryPath, filename); // file path
+
+            try
+            {
+                byte[] imageData = System.IO.File.ReadAllBytes(imagePath);
+                string contentType = "image/" + imageType; // file type
+
+                return new FileContentResult(imageData, contentType);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw new NotFoundException("image file not found");
+            }
+        }
+
+    }
+}
