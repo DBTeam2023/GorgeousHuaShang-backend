f59f5127d2a587f47122a93a186b5a0bcb67dd5a
うさぎ <momoyama.sawa@gmail.com>
2023-09-02 19:36:46 +0800

    feat: con

---
+++ Order/resource/controller/OrderController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Order.domain.model.repository.impl;
+using Order.common;
+using Order.domain.model;
+using EntityFramework.Context;
+using Order.utils;
+using Order.application;
+using Order.dto;
+using Order.resource.vo;
+
+namespace Order.resource.controller
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class OrderController : ControllerBase
+    {
+        //application service
+        public OrderApplicationService OrderApplicationService;
+        public OrderController(OrderApplicationService _OrderApplicationService)
+        {
+            OrderApplicationService = _OrderApplicationService;
+        }
+
+        //Authorization:seller
+        [HttpPost]
+        public async Task<ComResponse<OrderIdDto>> createOrder([FromBody] CreateOrderDto order)
+        {
+            return ComResponse<OrderIdDto>.success(await OrderApplicationService.createOrder(order));
+        }
+
+        //Authorization:seller
+        [HttpPost]
+        public ComResponse<OrderInfoVo> getOrderInfo([FromBody] OrderIdDto orderId)
+        {
+            var OrderAggrgate = OrderApplicationService.getOrderInfo(orderId.OrderId);
+            return ComResponse<OrderInfoVo>.success(new OrderInfoVo(OrderAggrgate));
+
+        }
+
+        //Authorization:seller
+        [HttpPost]
+        public async Task<ComResponse<string>> updateOrder([FromBody] OrderDto order)
+        {
+            await OrderApplicationService.UpdateOrder(order);
+            return ComResponse<string>.success("成功更新");
+        }
+
+        //Authorization:seller
+        //对某种商品的删除（全部删除）
+        [HttpPost]
+        public async Task<ComResponse<string>> deleteOrder([FromBody] OrderIdDto orderID)
+        {
+            await OrderApplicationService.DeleteOrder(orderID.OrderId);
+            return ComResponse<string>.success("成功删除");
+        }
+    }
+
+}
+
---
+++ Order/resource/vo/OrderInfoVo.cs
@@ -0,0 +1,35 @@
+﻿using Order.common;
+using Order.domain.model;
+
+namespace Order.domain.model
+{
+    public class OrderInfoVo
+    {
+        public string OrderId { get; set; } = null!;
+        public string CreateTime { get; set; } = null!;
+        public decimal Money { get; set; }
+        public int State { get; set; }
+        public string LogisticId { get; set; } = null!;
+        public string[] PickId { get; set; } = null!;
+        public string UserId { get; set; } = null!;
+        public string NickName { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public bool? IsDeleted { get; set; } = false;
+
+        public OrderInfoVo(OrderAggregate orderAggregate)
+        {
+            OrderId = orderAggregate.OrderID;
+            CreateTime = orderAggregate.CreateTime;
+            Money = orderAggregate.Money;
+            State = orderAggregate.State;
+            LogisticId = orderAggregate.LogisticID;
+            PickId = orderAggregate.PickID;
+            UserId = orderAggregate.UserID;
+            NickName = orderAggregate.NickName;
+            PhoneNumber = orderAggregate.PhoneNumber;
+            Address = orderAggregate.Address;
+            IsDeleted = orderAggregate.IsDeleted;
+        }
+    }
+}
